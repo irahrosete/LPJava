bb9fc9a0fa59086419f80dbe54e21001c9632c31
irah rosete <irah.rosete@outlook.com>
2021-12-10 12:14:03 +1100

    add awt example

---
+++ src/section11/Main.java
@@ -0,0 +1,8 @@
+package section11;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Java");
+        myWindow.setVisible(true);
+    }
+}
---
+++ src/section11/MyWindow.java
@@ -0,0 +1,31 @@
+package section11;
+
+//needs X11 DISPLAY to run
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyWindow extends Frame {
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("I am Learning Java", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("Irah Rosete", 60, 90);
+    }
+}
