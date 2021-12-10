1cbae446a8e927b9e6c6862abe3778f1baf0aa0b
irah rosete <irah.rosete@outlook.com>
2021-12-10 15:03:23 +1100

    implement nSum, factorial and fibonacci methods

--- src/section11/Main.java
+++ src/section11/Main.java
@@ -2,7 +2,10 @@ package section11;
 
 public class Main {
     public static void main(String[] args) {
-        MyWindow myWindow = new MyWindow("Java");
-        myWindow.setVisible(true);
+//        MyWindow myWindow = new MyWindow("Java");
+//        myWindow.setVisible(true);
+        System.out.println(Series.nSum(2));
+        System.out.println(Series.factorial(2));
+        System.out.println(Series.fibonacci(10));
     }
 }
---
+++ src/section11/Series.java
@@ -0,0 +1,71 @@
+package section11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Series {
+    private int n;
+
+    public Series(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public static int nSum(int n) {
+        if (n < 0) {
+            System.out.println("Enter a positive number");
+            return -1;
+        } else {
+            int nSumResult = 0;
+            for (int i = 0; i <= n; i++) {
+                nSumResult += i;
+            }
+            return nSumResult;
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n < 0) {
+            System.out.println("Enter a positive number");
+            return -1;
+        } else if (n == 0) {
+            return 0;
+        } else {
+            int factorialResult = 1;
+            for (int i = 1; i <= n; i++) {
+                factorialResult *= i;
+            }
+            return factorialResult;
+        }
+    }
+
+    public static int fibonacci(int n) {
+        List<Integer> fibonacciSequence = new ArrayList<>();
+        if (n < 0) {
+            System.out.println("Enter a positive number");
+            return -1;
+        } else {
+            fibonacciSequence.add(0);
+            fibonacciSequence.add(1);
+            if (n == 0 || n == 1) {
+                return fibonacciSequence.get(n);
+            } else {
+                for (int i=2; i<=n; i++) {
+                    int currentItem = fibonacciSequence.get(i-1);
+                    int previousItem = fibonacciSequence.get(i-2);
+                    int newItem = currentItem + previousItem;
+                    fibonacciSequence.add(i, newItem);
+                }
+            }
+            System.out.println(fibonacciSequence);
+            return fibonacciSequence.get(n);
+        }
+    }
+}
